746ca92373ad9228a0db4b9f12c2691068cd1b17
Kev Zettler <kevzettler@gmail.com>
2020-05-18 21:32:34 -0700

    add emscripten loader

--- make_emscripten.sh
+++ make_emscripten.sh
@@ -4,11 +4,11 @@ rm -rf emscripten/*
 mkdir -p emscripten
 
 cp -r ../flare-game/mods/fantasycore mods/fantasycore
-cp -r ../flare-game/mods/empyrean_campaign mods/empyrean_campaign
+cp -r ../flare-game/mods/kalorian_quest mods/kalorian_quest
 
 cmake .
 
-emcc \
+EMCC_DEBUG=1 emcc \
 	-v \
     -Isrc/ \
     src/*.cpp \
@@ -18,14 +18,14 @@ emcc \
     -s USE_SDL=2 \
     -s USE_SDL_IMAGE=2 \
     -s SDL2_IMAGE_FORMATS='["png"]' \
-	-s USE_SDL_TTF=2 \
-	-s USE_SDL_MIXER=2 \
-	-s "EXTRA_EXPORTED_RUNTIME_METHODS=['print']" \
-	-lidbfs.js \
+    -s USE_SDL_TTF=2 \
+    -s USE_SDL_MIXER=2 \
+    -s "EXTRA_EXPORTED_RUNTIME_METHODS=['print','printErr']" \
+    -lidbfs.js \
     --preload-file mods \
     -o emscripten/index.html
 
 rm -rf mods/fantasycore
-rm -rf mods/empyrean_campaign
+rm -rf mods/kalorian_quest
 
-cp distribution/emscripten_template.html emscripten/index.html
+cp distribution/good_emscripten_template.html emscripten/index.html
